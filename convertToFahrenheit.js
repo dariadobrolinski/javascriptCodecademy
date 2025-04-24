b563765427c1031e0b56e6ac46fcbce5e1a53bd1
dariadobrolinski <dsdobrolinski@gmail.com>
2025-04-24 15:57:02 -0400

    first commit

---
+++ convertToFahrenheit.js
@@ -0,0 +1,11 @@
+let kelvin = 293; // today's temp in kelvin
+
+// today's temp in celcius
+let celcius = kelvin - 273; 
+
+// today's temp in fahrenheit
+let fahrenheit = celcius * (9/5) + 32 
+// round down today's temp
+fahrenheit = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
