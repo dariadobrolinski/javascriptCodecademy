25c0c5c217ad511df883ee5f173feebc20237d72
dariadobrolinski <dsdobrolinski@gmail.com>
2025-04-28 11:50:42 -0400

    fix: add missing semicolon in console log statement

--- convertToFahrenheit.js
+++ convertToFahrenheit.js
@@ -8,4 +8,4 @@ let fahrenheit = celcius * (9/5) + 32
 // round down today's temp
 fahrenheit = Math.floor(fahrenheit)
 
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
