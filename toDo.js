8cf98ac0e8843446bd9da6268ee5bd8e445708bc
Daria Dobrolinski <dsdobrolinski@gmail.com>
2025-05-13 10:44:28 -0400

    feat: initialize toDo list with initial tasks

---
+++ toDo.js
@@ -0,0 +1,11 @@
+let toDo = [];
+
+todoList.push("Go to the gym");
+todoList.push("Study algorithms");
+todoList.push("Buy groceries");
+
+todoList.shift();
+
+todoList.push("Take a nap because you deserve it");
+
+console.log(todoList);
